c217cd602ae75c5f5ebbb73f8df30a1a658cdb1b
matteo <matteo-gz@users.noreply.github.com>
2023-12-27 23:38:42 +0800

    kratos hello

---
+++ internal/server/http.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	v1 "helloworld/api/helloworld/v1"
+	"helloworld/internal/conf"
+	"helloworld/internal/service"
+)
+
+func init() {
+	prometheus.MustRegister(_metricSeconds, _metricRequests)
+}
+
+var (
+	_metricSeconds = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "server",
+		Subsystem: "requests",
+		Name:      "duration_sec",
+		Help:      "server requests duration(sec).",
+		Buckets:   []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.250, 0.5, 1},
+	}, []string{"kind", "operation"})
+
+	_metricRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "client",
+		Subsystem: "requests",
+		Name:      "code_total",
+		Help:      "The total number of processed requests",
+	}, []string{"kind", "operation", "code", "reason"})
+)
+
+// NewHTTPServer new an HTTP server.
+func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *http.Server {
+	var opts = []http.ServerOption{
+		http.Middleware(
+			recovery.Recovery(),
+			metrics.Server(
+				metrics.WithSeconds(prom.NewHistogram(_metricSeconds)),
+				metrics.WithRequests(prom.NewCounter(_metricRequests)),
+			),
+		),
+	}
+	if c.Http.Network != "" {
+		opts = append(opts, http.Network(c.Http.Network))
+	}
+	if c.Http.Addr != "" {
+		opts = append(opts, http.Address(c.Http.Addr))
+	}
+	if c.Http.Timeout != nil {
+		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
+	}
+	srv := http.NewServer(opts...)
+	v1.RegisterGreeterHTTPServer(srv, greeter)
+	srv.Handle("/metrics", promhttp.Handler())
+	return srv
+}
